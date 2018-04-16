99c6dc48f4663d049210b916b6f665925975ab69
Manjunatha D <manjunathad13@gmail.com>
2018-04-17 00:21:29 +0530

    Basic routing

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('*', (request, response) => {
+    // send index.html for all the requests
+    response.sendFile('public/index.html', { root: __dirname });
+});
+
 app.listen(PORT, () => console.log(`App running at ${PORT}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
 
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        test: /\.s?css$/
-      }
-    ]
-  },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.s?css$/
+            }
+        ]
+    },
 
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+    },
 
-  devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map'
 };
